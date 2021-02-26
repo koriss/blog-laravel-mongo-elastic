6dcc543e66b6c4b8e7e18b2ba84d32b02b0e9304
Oleg Kazachkov <kazachkov@br-analytics.ru>
2021-02-26 18:11:52 +0300

    auth

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-Route::get('/', [App\Http\Controllers\Auth\UserController::class, 'index'])->name('show');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
